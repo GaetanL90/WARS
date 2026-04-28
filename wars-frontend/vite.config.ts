9eaa35f3284a3de09c649f0876d65326c641b022
GaetanL90 <gyitgaetan90@gmail.com>
2026-04-28 20:11:23 +0200

    --fix-7 location load

--- wars-frontend/vite.config.ts
+++ wars-frontend/vite.config.ts
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api-admin': {
+        target: 'https://rwanda-administrative-structure-api.onrender.com/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api-admin/, '')
+      }
+    }
+  }
 });
